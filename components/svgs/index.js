981b3fbc1b0e9ce9e62c5ab28b660303c646e170
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-03-12 07:09:08 -0600

    change logo navigation

---
+++ components/svgs/index.js
@@ -0,0 +1,4 @@
+export { default as DotsSVG } from './dots'
+export { default as LogoSVG } from './logo'
+export { default as MarkSVG } from './mark'
+export { default as Vitoz } from './vitoz'
---
+++ components/svgs/vitoz.js
@@ -0,0 +1,59 @@
+export default function Vitoz({ ...props }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="250"
+      zoomAndPan="magnify"
+      viewBox="0 0 187.5 37.5"
+      height="50"
+      preserveAspectRatio="xMidYMid meet"
+      version="1.0"
+      {...props}
+    >
+      <defs>
+        <clipPath id="id1">
+          <path
+            d="M 79 1.046875 L 100 1.046875 L 100 35.925781 L 79 35.925781 Z M 79 1.046875 "
+            clip-rule="nonzero"
+          />
+        </clipPath>
+        <clipPath id="id2">
+          <path
+            d="M 97 1.046875 L 133.726562 1.046875 L 133.726562 35.925781 L 97 35.925781 Z M 97 1.046875 "
+            clip-rule="nonzero"
+          />
+        </clipPath>
+        <clipPath id="id3">
+          <path
+            d="M 50.609375 1.046875 L 88 1.046875 L 88 35.925781 L 50.609375 35.925781 Z M 50.609375 1.046875 "
+            clip-rule="nonzero"
+          />
+        </clipPath>
+      </defs>
+      <g clip-path="url(#id1)">
+        <path
+          fill="rgb(14.118958%, 16.079712%, 18.429565%)"
+          d="M 99.183594 15.136719 L 87.171875 35.925781 L 79.015625 21.863281 L 91.046875 1.046875 L 99.183594 15.136719 "
+          fill-opacity="1"
+          fill-rule="nonzero"
+        />
+      </g>
+      <g clip-path="url(#id2)">
+        <path
+          fill="rgb(91.369629%, 23.919678%, 26.669312%)"
+          d="M 117.40625 1.046875 L 97.183594 35.925781 L 113.582031 35.925781 L 133.734375 1.046875 L 117.40625 1.046875 "
+          fill-opacity="1"
+          fill-rule="nonzero"
+        />
+      </g>
+      <g clip-path="url(#id3)">
+        <path
+          fill="rgb(91.369629%, 23.919678%, 26.669312%)"
+          d="M 50.617188 1.046875 L 70.773438 35.925781 L 87.171875 35.925781 L 66.949219 1.046875 L 50.617188 1.046875 "
+          fill-opacity="1"
+          fill-rule="nonzero"
+        />
+      </g>
+    </svg>
+  )
+}
