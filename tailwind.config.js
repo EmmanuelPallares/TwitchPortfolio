2c183461b875489540126b4a8bf09ba18be1154c
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-03-12 09:58:14 -0600

    added a hero componenet

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        'pink-background':
+          "url('https://cdn.tuk.dev/assets/pink-bg-gradient.jpg')"
+      }
+    }
   },
   plugins: []
 }
